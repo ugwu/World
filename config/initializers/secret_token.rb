aa397e677961651a646702859d06cc10faf26e2c
Patrick <patrick.ugwu@mit-can.com>
2011-03-20 21:57:58 +0000

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+World::Application.config.secret_token = '87bfda78003d5926fd4c29a405cc3991a5f89b078e02362246d8486e9125f12c9aadf62964211908543abc55cf3384995950324edafe904ffea1650337883698'
